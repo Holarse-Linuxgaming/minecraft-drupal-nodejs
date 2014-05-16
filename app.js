9b555467e865d368e6ab6c795df80d948100a683
commel <commel@gmail.com>
2014-05-16 10:28:59 +0000

    Von Express auf Restify umgestellt

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 var _ = require("underscore");
 var mysql = require("mysql");
-var express = require("express");
-var app = express();
+var restify = require("restify");
 
 var connection = mysql.createConnection({ 
   host: "localhost",
   user: "migration",
   password: "migrationpassword",
   database: "holarse"
+}).connect();
+
+var server = restify.createServer();
+server.get("/minecraft/allowed_users.json", function(req, res, next) {
+	connection.query("select * from minecraft_users;", function(err, rows) {
+    	var vals = _.map(rows, function(i) { return i.value; });
+    	console.log(vals);
+    	res.send(vals);
+  	});	 
 });
 
-connection.connect();
-
-app.get("/minecraft/allowed_users.json", function(req, res) {
-  connection.query("select * from minecraft_users;", function(err, rows) {
-    var vals = _.map(rows, function(i) { return i.value; });
-    console.log(vals);
-    res.send(vals);
-  });
-});
-
-var server = app.listen(4567, function() {
+var server = server.listen(4567, function() {
   console.log("Listening on port %d", server.address().port);
 });
